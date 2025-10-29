e74d5eb88b3b61588aca3c922c20ce230d4b396f
DavidVillanueva2 <David.Villanueva1@alu.uclm.es>
2025-10-29 19:41:53 +0100

    Update jugador.java

--- src/PracticaQueue/jugador.java
+++ src/PracticaQueue/jugador.java
@@ -1,9 +1,4 @@
-package QUEUE;
-
-import java.util.*;
-
-
-public class Jugador implements Comparable<Jugador> {
+public class Jugador implements DefineTipeT, Comparable<Jugador> {
     private String RequestID;
     private String PlayerID;
     private boolean PremiumSubscription;
@@ -22,19 +17,17 @@ public class Jugador implements Comparable<Jugador> {
         this.MatchType = matchType;
     }
 
-    
+    @Override
     public void readData(String line) {
         String[] a = line.split(";"); // separo las palabras por los ; que se ponen automaticamente al cambiar de celda al convertir desde excel
         this.RequestID = a[0];
         this.PlayerID = a[1];
         this.PremiumSubscription = Boolean.parseBoolean(a[2]); //convierto la palabra "true" o "false" en su valor como booleano
         this.SkillLevel = Integer.parseInt(a[3]); // convierto los numeros como String a integers. (originalmente toda la linea son Strings)
-        this.MatchType = a[4].charAt(); 
+        this.MatchType = a[4].charAt(0); 
     }
 
-    
-    public String getRequestID() {
-         return RequestID; }
+
     public String getPlayerID() {
          return PlayerID; }
     public boolean isPremium() {
